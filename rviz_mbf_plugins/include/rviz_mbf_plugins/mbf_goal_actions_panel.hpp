18c1452e082b1b5ceba5bdc0bce027707d54be83
Justus Braun <jubraun@uos.de>
2025-09-06 09:27:01 +0200

    Fix uncrustify

--- rviz_mbf_plugins/include/rviz_mbf_plugins/mbf_goal_actions_panel.hpp
+++ rviz_mbf_plugins/include/rviz_mbf_plugins/mbf_goal_actions_panel.hpp
@@ -62,7 +62,7 @@ class MbfGoalActionsPanel : public rviz_common::Panel
 
 public:
   explicit MbfGoalActionsPanel(QWidget * parent = nullptr);
-  virtual ~MbfGoalActionsPanel() {};
+  virtual ~MbfGoalActionsPanel() {}
 
   void onInitialize() override;
   void save(rviz_common::Config config) const override;
